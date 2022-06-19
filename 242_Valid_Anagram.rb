20da5f8047b2af6e73790d901641d5acecdf6f32
anne ju <anneju.824@gmail.com>
2022-06-19 20:56:50 +0800

    more Qs

---
+++ 242_Valid_Anagram.rb
@@ -0,0 +1,9 @@
+# 242. Valid Anagram
+# https://leetcode.com/problems/valid-anagram/
+
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+  s.chars.sort == t.chars.sort
+end
---
+++ 70_Climbing_Stairs.rb
@@ -0,0 +1,21 @@
+# 70. Climbing Stairs
+# https://leetcode.com/problems/climbing-stairs/
+
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+  # 1, 2, 3, 4, 5, ...
+  # 1, 2, 3, 5, 8, ...
+  return 1 if n == 1
+  return 2 if n == 2
+
+  a, b = 1, 2
+  index = 3
+  while index <= n
+    a, b = b, a + b
+    index += 1
+  end
+  b
+end
+
+climb_stairs(4)
